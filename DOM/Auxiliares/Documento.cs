f11a3d639fda899f088ad19911a7ecbb77815696
tferraro <tetferraro@gmail.com>
2014-09-20 22:41:29 -0300

    Agregado el proyecto de las clases de dominio

---
+++ DOM/Auxiliares/Documento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DOM.Auxiliares
+{
+    class Documento
+    {
+        public enum tipo { DNI, LC, LE, Pasaporte };
+        private tipo tipoDocumento;
+        private int numero;
+
+        public int nroDoc
+        {
+            get {return numero; }
+            set {numero = value; }
+        }
+        public tipo tipoDoc
+        {
+            get {return tipoDocumento;}
+            set { tipoDocumento = value;}
+
+    }
+}
---
+++ DOM/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DOM.Auxiliares;
+
+namespace DOM
+{
+    public class Usuario
+    {
+        #region Atributos Privados
+        private string usr;
+        //Password
+        private string nombre;
+        private string apellido;
+        //Mail
+        private Documento documento;
+        //Mail
+        //Telefono
+        //Direccion
+        //FechaNacimiento
+        //Hoteles
+        //Roles
+        #endregion
+
+    }
+}
