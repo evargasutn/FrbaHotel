d7a6e45533c9da1f2d43ea6c510b10dcefc150db
n.e.ledesma@hotmail.com <n.e.ledesma@hotmail>
2014-10-21 23:11:06 -0300

    Modificacion de interfaz de reserva

--- FrbaHotel/Cancelar Reserva/BuscarIdHuesped.Designer.cs
+++ FrbaHotel/Cancelar Reserva/BuscarIdHuesped.Designer.cs
@@ -30,17 +30,20 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textNombre = new System.Windows.Forms.TextBox();
-            this.textApellido = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.textNroDocumento = new System.Windows.Forms.TextBox();
             this.comboTipoDocumento = new System.Windows.Forms.ComboBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textApellido = new System.Windows.Forms.TextBox();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridHuesped = new System.Windows.Forms.DataGridView();
+            this.ColumnSeleccionar = new System.Windows.Forms.DataGridViewButtonColumn();
             this.botonLimpiar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridHuesped)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -62,59 +65,12 @@
             this.groupBox1.Controls.Add(this.textNombre);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(29, 21);
+            this.groupBox1.Location = new System.Drawing.Point(100, 27);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(266, 130);
+            this.groupBox1.Size = new System.Drawing.Size(299, 130);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Filtros para buscar huesped";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 48);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Apellido";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 99);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(115, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Número de documento";
-            // 
-            // textNombre
-            // 
-            this.textNombre.Location = new System.Drawing.Point(127, 22);
-            this.textNombre.Name = "textNombre";
-            this.textNombre.Size = new System.Drawing.Size(121, 20);
-            this.textNombre.TabIndex = 3;
-            // 
-            // textApellido
-            // 
-            this.textApellido.Location = new System.Drawing.Point(127, 45);
-            this.textApellido.Name = "textApellido";
-            this.textApellido.Size = new System.Drawing.Size(121, 20);
-            this.textApellido.TabIndex = 4;
-            // 
-            // textNroDocumento
-            // 
-            this.textNroDocumento.Location = new System.Drawing.Point(127, 96);
-            this.textNroDocumento.Name = "textNroDocumento";
-            this.textNroDocumento.Size = new System.Drawing.Size(121, 20);
-            this.textNroDocumento.TabIndex = 5;
-            // 
-            // comboTipoDocumento
-            // 
-            this.comboTipoDocumento.FormattingEnabled = true;
-            this.comboTipoDocumento.Location = new System.Drawing.Point(127, 71);
-            this.comboTipoDocumento.Name = "comboTipoDocumento";
-            this.comboTipoDocumento.Size = new System.Drawing.Size(121, 21);
-            this.comboTipoDocumento.TabIndex = 6;
             // 
             // label4
             // 
@@ -125,36 +81,102 @@
             this.label4.TabIndex = 7;
             this.label4.Text = "Tipo de documento";
             // 
-            // dataGridView1
+            // textNroDocumento
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(29, 157);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(342, 144);
-            this.dataGridView1.TabIndex = 2;
+            this.textNroDocumento.Location = new System.Drawing.Point(127, 96);
+            this.textNroDocumento.Name = "textNroDocumento";
+            this.textNroDocumento.Size = new System.Drawing.Size(164, 20);
+            this.textNroDocumento.TabIndex = 5;
+            // 
+            // comboTipoDocumento
+            // 
+            this.comboTipoDocumento.FormattingEnabled = true;
+            this.comboTipoDocumento.Location = new System.Drawing.Point(127, 71);
+            this.comboTipoDocumento.Name = "comboTipoDocumento";
+            this.comboTipoDocumento.Size = new System.Drawing.Size(164, 21);
+            this.comboTipoDocumento.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Número de documento";
+            // 
+            // textApellido
+            // 
+            this.textApellido.Location = new System.Drawing.Point(127, 45);
+            this.textApellido.Name = "textApellido";
+            this.textApellido.Size = new System.Drawing.Size(164, 20);
+            this.textApellido.TabIndex = 4;
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(127, 22);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(164, 20);
+            this.textNombre.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido";
+            // 
+            // dataGridHuesped
+            // 
+            this.dataGridHuesped.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridHuesped.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnSeleccionar});
+            this.dataGridHuesped.Location = new System.Drawing.Point(11, 19);
+            this.dataGridHuesped.Name = "dataGridHuesped";
+            this.dataGridHuesped.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.dataGridHuesped.Size = new System.Drawing.Size(442, 153);
+            this.dataGridHuesped.TabIndex = 2;
+            // 
+            // ColumnSeleccionar
+            // 
+            this.ColumnSeleccionar.HeaderText = "Seleccionar";
+            this.ColumnSeleccionar.Name = "ColumnSeleccionar";
             // 
             // botonLimpiar
             // 
-            this.botonLimpiar.Location = new System.Drawing.Point(69, 310);
+            this.botonLimpiar.Location = new System.Drawing.Point(207, 357);
             this.botonLimpiar.Name = "botonLimpiar";
             this.botonLimpiar.Size = new System.Drawing.Size(114, 27);
             this.botonLimpiar.TabIndex = 3;
             this.botonLimpiar.Text = "Limpiar";
             this.botonLimpiar.UseVisualStyleBackColor = true;
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridHuesped);
+            this.groupBox2.Location = new System.Drawing.Point(18, 163);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(468, 188);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Huespedes";
+            // 
             // BuscarIdHuesped
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 349);
+            this.ClientSize = new System.Drawing.Size(499, 397);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.botonLimpiar);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.groupBox1);
             this.Name = "BuscarIdHuesped";
             this.Text = "Busqueda de identificación";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridHuesped)).EndInit();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -170,7 +192,9 @@
         private System.Windows.Forms.TextBox textApellido;
         private System.Windows.Forms.TextBox textNombre;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridHuesped;
         private System.Windows.Forms.Button botonLimpiar;
+        private System.Windows.Forms.DataGridViewButtonColumn ColumnSeleccionar;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
--- FrbaHotel/Cancelar Reserva/BuscarReserva.Designer.cs
+++ FrbaHotel/Cancelar Reserva/BuscarReserva.Designer.cs
@@ -29,113 +29,161 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBoxCodReserva = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.dateTimeFechaReserva = new System.Windows.Forms.DateTimePicker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dateTimeFin = new System.Windows.Forms.DateTimePicker();
             this.label3 = new System.Windows.Forms.Label();
+            this.dateTimeFechaInicio = new System.Windows.Forms.DateTimePicker();
             this.botonBuscarIdHuesped = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridReserva = new System.Windows.Forms.DataGridView();
+            this.ColumnSeleccionar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.label4 = new System.Windows.Forms.Label();
+            this.botonLimpiar = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridReserva)).BeginInit();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Controls.Add(this.botonBuscarIdHuesped);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.dateTimeFechaReserva);
             this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.textBoxCodReserva);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(21, 12);
+            this.groupBox1.Location = new System.Drawing.Point(27, 19);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(341, 123);
+            this.groupBox1.Size = new System.Drawing.Size(341, 160);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Filtros";
             // 
-            // textBoxCodReserva
+            // groupBox2
             // 
-            this.textBoxCodReserva.Location = new System.Drawing.Point(139, 24);
-            this.textBoxCodReserva.Name = "textBoxCodReserva";
-            this.textBoxCodReserva.Size = new System.Drawing.Size(133, 20);
-            this.textBoxCodReserva.TabIndex = 2;
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.dateTimeFin);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.dateTimeFechaInicio);
+            this.groupBox2.Location = new System.Drawing.Point(17, 55);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(311, 91);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Fecha de reserva";
             // 
-            // label2
+            // dateTimeFin
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(31, 56);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Id huesped";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 26);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Codigo de reserva";
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(21, 176);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(328, 144);
-            this.dataGridView1.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(139, 53);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(133, 20);
-            this.textBox1.TabIndex = 3;
-            // 
-            // dateTimeFechaReserva
-            // 
-            this.dateTimeFechaReserva.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimeFechaReserva.Location = new System.Drawing.Point(139, 79);
-            this.dateTimeFechaReserva.Name = "dateTimeFechaReserva";
-            this.dateTimeFechaReserva.Size = new System.Drawing.Size(133, 20);
-            this.dateTimeFechaReserva.TabIndex = 4;
+            this.dateTimeFin.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimeFin.Location = new System.Drawing.Point(54, 43);
+            this.dateTimeFin.Name = "dateTimeFin";
+            this.dateTimeFin.Size = new System.Drawing.Size(133, 20);
+            this.dateTimeFin.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(31, 86);
+            this.label3.Location = new System.Drawing.Point(16, 23);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.Size = new System.Drawing.Size(32, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Fecha de reserva";
+            this.label3.Text = "Inicio";
+            // 
+            // dateTimeFechaInicio
+            // 
+            this.dateTimeFechaInicio.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimeFechaInicio.Location = new System.Drawing.Point(54, 17);
+            this.dateTimeFechaInicio.Name = "dateTimeFechaInicio";
+            this.dateTimeFechaInicio.Size = new System.Drawing.Size(133, 20);
+            this.dateTimeFechaInicio.TabIndex = 4;
             // 
             // botonBuscarIdHuesped
             // 
-            this.botonBuscarIdHuesped.Location = new System.Drawing.Point(278, 53);
+            this.botonBuscarIdHuesped.Location = new System.Drawing.Point(278, 19);
             this.botonBuscarIdHuesped.Name = "botonBuscarIdHuesped";
             this.botonBuscarIdHuesped.Size = new System.Drawing.Size(50, 19);
             this.botonBuscarIdHuesped.TabIndex = 6;
             this.botonBuscarIdHuesped.Text = "Buscar";
             this.botonBuscarIdHuesped.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(133, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Id huesped";
+            // 
+            // dataGridReserva
+            // 
+            this.dataGridReserva.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridReserva.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnSeleccionar});
+            this.dataGridReserva.Location = new System.Drawing.Point(7, 19);
+            this.dataGridReserva.Name = "dataGridReserva";
+            this.dataGridReserva.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.dataGridReserva.Size = new System.Drawing.Size(368, 167);
+            this.dataGridReserva.TabIndex = 1;
+            // 
+            // ColumnSeleccionar
+            // 
+            this.ColumnSeleccionar.HeaderText = "Seleccionar";
+            this.ColumnSeleccionar.Name = "ColumnSeleccionar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Fin";
+            // 
+            // botonLimpiar
+            // 
+            this.botonLimpiar.Location = new System.Drawing.Point(147, 377);
+            this.botonLimpiar.Name = "botonLimpiar";
+            this.botonLimpiar.Size = new System.Drawing.Size(107, 26);
+            this.botonLimpiar.TabIndex = 2;
+            this.botonLimpiar.Text = "Limpiar";
+            this.botonLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dataGridReserva);
+            this.groupBox3.Location = new System.Drawing.Point(5, 185);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(381, 192);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Reservas del huesped";
+            // 
             // BuscarReserva
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(423, 332);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(391, 435);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.botonLimpiar);
             this.Controls.Add(this.groupBox1);
             this.Name = "BuscarReserva";
             this.Text = "Busqueda de reserva";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridReserva)).EndInit();
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -143,13 +191,17 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridReserva;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBoxCodReserva;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button botonBuscarIdHuesped;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.DateTimePicker dateTimeFechaReserva;
+        private System.Windows.Forms.DateTimePicker dateTimeFechaInicio;
+        private System.Windows.Forms.DataGridViewButtonColumn ColumnSeleccionar;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DateTimePicker dateTimeFin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button botonLimpiar;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
