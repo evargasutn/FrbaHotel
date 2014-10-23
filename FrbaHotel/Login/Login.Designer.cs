2b26bb57c393316400dfec748290dc7b5493e649
n.e.ledesma@hotmail.com <n.e.ledesma@hotmail>
2014-10-23 15:42:50 -0300

    modificacion login

--- FrbaHotel/Login/Login.Designer.cs
+++ FrbaHotel/Login/Login.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace FrbaHotel
+﻿namespace FrbaHotel.Login
 {
-    partial class Inicial
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,79 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.botonIniciarSesion = new System.Windows.Forms.Button();
-            this.botonInvitado = new System.Windows.Forms.Button();
-            this.lblUsuario = new System.Windows.Forms.Label();
-            this.lblContrasenia = new System.Windows.Forms.Label();
-            this.textUsuario = new System.Windows.Forms.TextBox();
             this.textContrasenia = new System.Windows.Forms.TextBox();
+            this.textUsuario = new System.Windows.Forms.TextBox();
+            this.lblContrasenia = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.botonInvitado = new System.Windows.Forms.Button();
+            this.botonIniciarSesion = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // botonIniciarSesion
+            // textContrasenia
             // 
-            this.botonIniciarSesion.Location = new System.Drawing.Point(32, 123);
-            this.botonIniciarSesion.Name = "botonIniciarSesion";
-            this.botonIniciarSesion.Size = new System.Drawing.Size(105, 31);
-            this.botonIniciarSesion.TabIndex = 0;
-            this.botonIniciarSesion.Text = "Iniciar Sesión";
-            this.botonIniciarSesion.UseVisualStyleBackColor = true;
-            this.botonIniciarSesion.Click += new System.EventHandler(this.button1_Click);
+            this.textContrasenia.Location = new System.Drawing.Point(132, 58);
+            this.textContrasenia.Name = "textContrasenia";
+            this.textContrasenia.Size = new System.Drawing.Size(108, 20);
+            this.textContrasenia.TabIndex = 11;
             // 
-            // botonInvitado
+            // textUsuario
             // 
-            this.botonInvitado.Location = new System.Drawing.Point(165, 123);
-            this.botonInvitado.Name = "botonInvitado";
-            this.botonInvitado.Size = new System.Drawing.Size(95, 31);
-            this.botonInvitado.TabIndex = 1;
-            this.botonInvitado.Text = "Invitado";
-            this.botonInvitado.UseVisualStyleBackColor = true;
-            // 
-            // lblUsuario
-            // 
-            this.lblUsuario.AutoSize = true;
-            this.lblUsuario.Location = new System.Drawing.Point(38, 51);
-            this.lblUsuario.Name = "lblUsuario";
-            this.lblUsuario.Size = new System.Drawing.Size(99, 13);
-            this.lblUsuario.TabIndex = 2;
-            this.lblUsuario.Text = "Nombre de usuario:";
+            this.textUsuario.Location = new System.Drawing.Point(132, 28);
+            this.textUsuario.Name = "textUsuario";
+            this.textUsuario.Size = new System.Drawing.Size(108, 20);
+            this.textUsuario.TabIndex = 10;
             // 
             // lblContrasenia
             // 
             this.lblContrasenia.AutoSize = true;
-            this.lblContrasenia.Location = new System.Drawing.Point(73, 77);
+            this.lblContrasenia.Location = new System.Drawing.Point(53, 61);
             this.lblContrasenia.Name = "lblContrasenia";
             this.lblContrasenia.Size = new System.Drawing.Size(64, 13);
-            this.lblContrasenia.TabIndex = 3;
+            this.lblContrasenia.TabIndex = 9;
             this.lblContrasenia.Text = "Contraseña:";
             // 
-            // textUsuario
+            // lblUsuario
             // 
-            this.textUsuario.Location = new System.Drawing.Point(152, 44);
-            this.textUsuario.Name = "textUsuario";
-            this.textUsuario.Size = new System.Drawing.Size(108, 20);
-            this.textUsuario.TabIndex = 4;
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(18, 35);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(99, 13);
+            this.lblUsuario.TabIndex = 8;
+            this.lblUsuario.Text = "Nombre de usuario:";
             // 
-            // textContrasenia
+            // botonInvitado
             // 
-            this.textContrasenia.Location = new System.Drawing.Point(152, 74);
-            this.textContrasenia.Name = "textContrasenia";
-            this.textContrasenia.Size = new System.Drawing.Size(108, 20);
-            this.textContrasenia.TabIndex = 5;
+            this.botonInvitado.Location = new System.Drawing.Point(145, 107);
+            this.botonInvitado.Name = "botonInvitado";
+            this.botonInvitado.Size = new System.Drawing.Size(95, 31);
+            this.botonInvitado.TabIndex = 7;
+            this.botonInvitado.Text = "Invitado";
+            this.botonInvitado.UseVisualStyleBackColor = true;
             // 
-            // Inicial
+            // botonIniciarSesion
+            // 
+            this.botonIniciarSesion.Location = new System.Drawing.Point(12, 107);
+            this.botonIniciarSesion.Name = "botonIniciarSesion";
+            this.botonIniciarSesion.Size = new System.Drawing.Size(105, 31);
+            this.botonIniciarSesion.TabIndex = 6;
+            this.botonIniciarSesion.Text = "Iniciar Sesión";
+            this.botonIniciarSesion.UseVisualStyleBackColor = true;
+            this.botonIniciarSesion.Click += new System.EventHandler(this.botonIniciarSesion_Click);
+            // 
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 181);
+            this.ClientSize = new System.Drawing.Size(253, 163);
             this.Controls.Add(this.textContrasenia);
             this.Controls.Add(this.textUsuario);
             this.Controls.Add(this.lblContrasenia);
             this.Controls.Add(this.lblUsuario);
             this.Controls.Add(this.botonInvitado);
             this.Controls.Add(this.botonIniciarSesion);
-            this.Name = "Inicial";
-            this.Text = "Inicio de Sesión";
-            this.Load += new System.EventHandler(this.Inicial_Load);
+            this.Name = "Login";
+            this.Text = "Login";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,12 +107,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button botonIniciarSesion;
-        private System.Windows.Forms.Button botonInvitado;
-        private System.Windows.Forms.Label lblUsuario;
-        private System.Windows.Forms.Label lblContrasenia;
-        private System.Windows.Forms.TextBox textUsuario;
         private System.Windows.Forms.TextBox textContrasenia;
+        private System.Windows.Forms.TextBox textUsuario;
+        private System.Windows.Forms.Label lblContrasenia;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Button botonInvitado;
+        private System.Windows.Forms.Button botonIniciarSesion;
     }
 }
-
--- FrbaHotel/Login/LoginRequisitos.Designer.cs
+++ FrbaHotel/Login/LoginRequisitos.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace FrbaHotel
+﻿namespace FrbaHotel.Login
 {
-    partial class Form2
+    partial class LoginRequisitos
     {
         /// <summary>
         /// Required designer variable.
@@ -28,60 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.botonAceptar = new System.Windows.Forms.Button();
-            this.listRoles = new System.Windows.Forms.ListBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.listHoteles = new System.Windows.Forms.ListBox();
-            this.groupBox1.SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listRoles = new System.Windows.Forms.ListBox();
+            this.botonAceptar = new System.Windows.Forms.Button();
             this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // botonAceptar
-            // 
-            this.botonAceptar.Location = new System.Drawing.Point(130, 181);
-            this.botonAceptar.Name = "botonAceptar";
-            this.botonAceptar.Size = new System.Drawing.Size(97, 29);
-            this.botonAceptar.TabIndex = 2;
-            this.botonAceptar.Text = "Aceptar";
-            this.botonAceptar.UseVisualStyleBackColor = true;
-            // 
-            // listRoles
-            // 
-            this.listRoles.FormattingEnabled = true;
-            this.listRoles.Location = new System.Drawing.Point(18, 19);
-            this.listRoles.Name = "listRoles";
-            this.listRoles.Size = new System.Drawing.Size(129, 95);
-            this.listRoles.TabIndex = 3;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.listRoles);
-            this.groupBox1.Location = new System.Drawing.Point(180, 49);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(161, 126);
-            this.groupBox1.TabIndex = 4;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Roles";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(338, 16);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Seleccione el Hotel al que quiere entrar y el rol.";
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.listHoteles);
-            this.groupBox2.Location = new System.Drawing.Point(13, 49);
+            this.groupBox2.Location = new System.Drawing.Point(12, 50);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(161, 126);
-            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabIndex = 8;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Hoteles";
             // 
@@ -93,19 +56,56 @@
             this.listHoteles.Size = new System.Drawing.Size(129, 95);
             this.listHoteles.TabIndex = 3;
             // 
-            // Form2
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(338, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Seleccione el Hotel al que quiere entrar y el rol.";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listRoles);
+            this.groupBox1.Location = new System.Drawing.Point(179, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(161, 126);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Roles";
+            // 
+            // listRoles
+            // 
+            this.listRoles.FormattingEnabled = true;
+            this.listRoles.Location = new System.Drawing.Point(18, 19);
+            this.listRoles.Name = "listRoles";
+            this.listRoles.Size = new System.Drawing.Size(129, 95);
+            this.listRoles.TabIndex = 3;
+            // 
+            // botonAceptar
+            // 
+            this.botonAceptar.Location = new System.Drawing.Point(129, 182);
+            this.botonAceptar.Name = "botonAceptar";
+            this.botonAceptar.Size = new System.Drawing.Size(97, 29);
+            this.botonAceptar.TabIndex = 6;
+            this.botonAceptar.Text = "Aceptar";
+            this.botonAceptar.UseVisualStyleBackColor = true;
+            // 
+            // LoginRequisitos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(359, 222);
+            this.ClientSize = new System.Drawing.Size(349, 217);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.botonAceptar);
-            this.Name = "Form2";
-            this.Text = "Seleccion del login";
-            this.groupBox1.ResumeLayout(false);
+            this.Name = "LoginRequisitos";
+            this.Text = "LoginRequisitos";
             this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,11 +113,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button botonAceptar;
-        private System.Windows.Forms.ListBox listRoles;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.ListBox listHoteles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listRoles;
+        private System.Windows.Forms.Button botonAceptar;
     }
 }
