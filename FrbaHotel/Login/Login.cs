0a46d69cb9f6ea56c0b066cc4cd207650c9bc8de
n.e.ledesma@hotmail.com <n.e.ledesma@hotmail.com>
2014-10-23 23:33:31 -0300

    Se agrego en el login la func conversion a hash

--- FrbaHotel/Login/Login.cs
+++ FrbaHotel/Login/Login.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using System.Security.Cryptography;
 namespace FrbaHotel.Login
 {
     public partial class Login : Form
@@ -15,10 +15,33 @@ namespace FrbaHotel.Login
         {
             InitializeComponent();
         }
-       
+
         private void botonIniciarSesion_Click(object sender, EventArgs e)
         {
-            
+
         }
+
+
+
+        public string encriptarASha256(string input)
+        {
+            SHA256CryptoServiceProvider provider = new SHA256CryptoServiceProvider();
+            ///transforma a secuencia de bytes,codificacion
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            ///calcula a valor hash
+            byte[] hashedBytes = provider.ComputeHash(inputBytes);
+            /// cadena de caracteres modificable
+            StringBuilder output = new StringBuilder();
+            ///formatea como string hex,anexa el valor hash.
+            for (int i = 0; i < hashedBytes.Length; i++)
+                output.Append(hashedBytes[i].ToString("x2").ToLower());
+            return output.ToString();
+        }
+
+
+
+
+
+
     }
 }
