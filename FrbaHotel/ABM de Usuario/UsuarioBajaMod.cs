43e91e54325fc5f0a874bb188afb6540a9c04662
vicgolob <victorgolob@gmail.com>
2014-11-03 02:56:58 -0300

    update BajaMod de Usuario

--- FrbaHotel/ABM de Usuario/UsuarioBajaMod.cs
+++ FrbaHotel/ABM de Usuario/UsuarioBajaMod.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using DOM;
 
 namespace FrbaHotel.ABM_de_Usuario
 {
@@ -14,6 +15,7 @@ namespace FrbaHotel.ABM_de_Usuario
         public FormModBajaUser()
         {
             InitializeComponent();
+
         }
 
         private void botonLimpiar_Click(object sender, EventArgs e)
@@ -38,9 +40,44 @@ namespace FrbaHotel.ABM_de_Usuario
 
         private void botonBuscar_Click(object sender, EventArgs e)
         {
-            //////
-            ///// completar el datagridview de usarios
-            /////
+            dataGridUsuario.DataSource = DAOUsuario.obtenerTabla("");
+            for (int item = 6; item < dataGridUsuario.ColumnCount; item++)
+            {
+                dataGridUsuario.Columns[item].Visible = false;
+            }
         }
+
+        private void btnAltaUsuario_Click(object sender, EventArgs e)
+        {
+            UsuarioAlta usrAlta = new UsuarioAlta();
+            usrAlta.Show(this);
+        }
+
+        private void dataGridUsuario_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridUsuario.Columns[e.ColumnIndex].HeaderText == "Modificar" )
+            {
+                FormModUser usrMod = new FormModUser();
+                MessageBox.Show("Falta autocompletar con los datos del usr");
+                usrMod.Show(this);
+            }
+
+            if (dataGridUsuario.Columns[e.ColumnIndex].HeaderText == "Baja")
+            {
+                string usrDelete = dataGridUsuario.Rows[e.RowIndex].Cells["usr"].FormattedValue.ToString();
+                DialogResult dr = MessageBox.Show("Desea dar de Baja al usuario "+usrDelete+"?",
+                      "", MessageBoxButtons.YesNo);
+                switch (dr)
+                {
+                    case DialogResult.Yes: 
+                        DAOUsuario.borrar(usrDelete); 
+                        break;
+                    case DialogResult.No: break;
+                }
+                
+            }
+        }
+
+        
     }
 }
